7c8ee8c1d4ea0817685597efc7890f81070f9e61
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2024-08-14 10:54:50 +0700

    fix docker issue

--- grafana/run.sh
+++ grafana/run.sh
@@ -1,4 +1,4 @@
 sudo chown -R 472:472 ./
 sudo chmod -R 755 ./
 
-DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose up --build --platform linux/amd64
+DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose up --build -d
